af776bc9d72ffea4b10e6a67f3d7c2c9111a954c
DjennadRany <128905298+DjennadRany@users.noreply.github.com>
2023-04-18 19:18:29 +0200

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+var rename = require("gulp-rename");
+const concat = require('gulp-concat');
 
 function buildStyles() {
-  return gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
-    
+  return gulp.src(['./sass/**/*.scss'])
+    .pipe(sass())
+    .pipe(concat('all.scss'))
+    .pipe(rename("style.css"))
+  .pipe(gulp.dest("css/"));
+
  
 };
 
 exports.default = buildStyles;
+
 exports.watch = function () {
-    gulp.watch('./sass/**/*.scss');
-  
+    gulp.watch(['./sass/**/*.scss']);
+ 
   };
